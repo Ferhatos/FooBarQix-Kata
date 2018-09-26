623f929057b62de3f83e4c57c273ec39275f16e6
Ferhat-G <ferhat.ghebghoub@ferobase.com>
2018-09-26 17:40:20 +0100

    simple main with no arguments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,13 +2,7 @@ import kata.FooBarQix;
 
 public class Main {
     public static void main(String[] args) {
-        int size = 100;
-        try {
-            size = Integer.parseInt(args[0]);
-        } catch (Exception e) {
-            System.out.println("Running without argument, default range size is 100");
-        }
         FooBarQix fooBarQix = new FooBarQix();
-        fooBarQix.processRange(size);
+        fooBarQix.processRange(100);
     }
 }
